8816bc1e9ed725f5c5105337daae6105d7d56c77
Andrew <35283726+switch900@users.noreply.github.com>
2019-03-29 23:03:36 -0700

    Add files via upload

---
+++ WpfApplication1/EditTitleBasicForm.xaml.cs
@@ -0,0 +1,169 @@
+﻿using IMDbDotNetAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace WpfApplication1
+{
+
+    /// <summary>
+    /// Interaction logic for EditTitleBasicForm.xaml
+    /// </summary>
+    public partial class EditTitleBasicForm : Window
+    {
+        public ObservableCollection<titlebasic> ObserveTitleBasicList { get; set; }
+        public static List<titlebasic> TitleBasicList;
+        private string titleBasicID;
+
+        public EditTitleBasicForm()
+        {
+            Run();
+        }
+
+        //starts UI and populates it.  
+        public void Run()
+        {
+            InitializeComponent();
+        }
+
+        //runs method to initialize HTTPClient
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BindTitlebasicsList();
+        }
+
+        //add baseAddress and default request headers to static HTTPClient
+        private void BindTitlebasicsList()
+        {
+            MainWindow.Client.BaseAddress = new Uri("http://localhost:2105/");
+            MainWindow.Client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/xml"));
+        }
+
+        //populates UI with a selected search criteria. 
+        public void GetTitleBasicByID(string id)
+        {
+            titleBasicID = id;
+            var url = "api/titlebasics/" + titleBasicID;
+            HttpResponseMessage response = MainWindow.Client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var titlebasics = response.Content.ReadAsAsync<IEnumerable<titlebasic>>().Result;
+                foreach (titlebasic temp in titlebasics)
+                {
+                    ObserveTitleBasicList = new ObservableCollection<titlebasic>(titlebasics);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+            DataContext = ObserveTitleBasicList;
+            Show();
+        }
+
+        //Saves changes and closes UI. 
+        private async void SaveClose_Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            if (titleBasicID != null)
+            {
+                await UpdateTitleBasicByIdAsync(titleBasicID);
+            }
+            else
+            {
+                await NewTitleBasic();
+            }
+            Close();
+        }
+
+        //saves changes but UI remains active.
+        private async void Save_Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            if (titleBasicID != null)
+            {
+                await UpdateTitleBasicByIdAsync(titleBasicID);
+            }
+            else
+            {
+                await NewTitleBasic();
+
+            }
+        }
+
+        //reloads page with original information from the last save.
+        private void Reset_Button_Click(object sender, RoutedEventArgs e)
+        {
+            GetTitleBasicByID(titleBasicID);
+        }
+
+        //creates new entry and saves it to databse from input from user.
+        private async Task NewTitleBasic()
+        {
+            isAdultComboBox.SelectedValue = "False";
+            titlebasic titlebasic = NewUser();        
+            var response = await MainWindow.Client.PostAsXmlAsync("api/titlebasics", titlebasic);
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Titlebasic Added");
+                Run();
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+            titleBasicID = tconstTextBox.Text;
+        }
+
+        //updates employee information in database on selected employee
+        private async Task UpdateTitleBasicByIdAsync(string id)
+        {
+            var url = "api/titlebasics/" + id;
+            titlebasic titlebasic = NewUser();
+            HttpResponseMessage response = await MainWindow.Client.PutAsXmlAsync(url, titlebasic);
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Titlebasic updated");
+                Run();
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+        //create new titlebasic object
+        private titlebasic NewUser()
+        {
+            bool ifSuccessIsAdult = false;
+            bool ifSuccessStartYear = short.TryParse(startYearTextBox.Text, out short tempStartYear);
+            bool ifSuccessEndYear = short.TryParse(endYearTextBox.Text, out short tempEndYear);
+            bool ifSuccessRunTimeMinutes = int.TryParse(runTimeMinutesTextBox.Text, out int tempRunTimeMinutes);
+
+            if (isAdultComboBox.Text.Equals("True") || isAdultComboBox.Text.Equals("False"))
+            {
+                ifSuccessIsAdult = Convert.ToBoolean(isAdultComboBox.Text);
+            }
+            var titlebasic = new titlebasic
+            {
+                tconst = tconstTextBox.Text,
+                titleType = titleTypeTextBox.Text,
+                primaryTitle = primaryTitleTextBox.Text,
+                originalTitle = originaltitleTextBox.Text,
+                isAdult = ifSuccessIsAdult,
+                startYear = tempStartYear,
+                endYear = tempEndYear,
+                runtimeMinutes = tempRunTimeMinutes,
+                genres = genreTextBox.Text
+            };
+
+            return titlebasic;
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using IMDbDotNetAPI.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<titlebasic> ObserveTitleBasicList { get; set; }
+        public static List<titlebasic> titleBasicList;
+        private string titleBasicID = null;
+        private int pageSize = 12;
+        private int pageIndex = 0;
+        public int ienumerableCount=0;
+        public List<titlebasic> asList;
+        public List<titlebasic> pageList = new List<titlebasic>();
+        public static HttpClient Client = new HttpClient();
+
+        //start window
+        public MainWindow()
+        {
+            Run();
+        }
+
+        //initialize components
+        public void Run()
+        {
+            InitializeComponent();
+        }
+
+        //run httpclient initialization method
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BindListAsync();        
+        }
+        
+        //add baseAddress and default request headers to static HTTPClient
+        private void BindListAsync()
+        {
+            Client.BaseAddress = new Uri("http://localhost:2105/");
+            Client.DefaultRequestHeaders.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/xml"));
+        }
+
+        //create new blank Edit dialog and display
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            EditTitleBasicForm edit = new EditTitleBasicForm();
+            edit.Show();
+        }
+
+        //create new Edit dialog and populate it with selected titlebasic object and display 
+        private void Edit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (titleBasicID != null)
+            {
+                EditTitleBasicForm edit = new EditTitleBasicForm();
+                edit.GetTitleBasicByID(titleBasicID);
+                edit.Show();
+            }
+        }
+
+        //doesn't do anything.  Save function is on EditTitleBasicForm...not here
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Saved");
+        }
+
+        //clears searchbox, page number, total count and creates new empty list and re-runs window
+        private void Refresh_Button_Click(object sender, RoutedEventArgs e)
+        {
+            m_txtTest.Text = "";
+            PageNumber.Content = "";
+            TotalCount.Content = "";
+            titleBasicList = new List<titlebasic>();
+            ObserveTitleBasicList = new ObservableCollection<titlebasic>(titleBasicList);
+            DataContext = ObserveTitleBasicList;
+            Run();
+        }
+
+        //deletes selected entry from database as per tconst
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var id = titleBasicID;
+
+            var url = "api/titlebasics/" + id;
+
+            HttpResponseMessage response = await Client.DeleteAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("You have deleted " + titleBasicID);
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+        //calls search method when button clicked
+        private async void btnSearch_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            await Search();
+        }
+
+        //calls search method when user double clicks on search box
+        private async void M_txtTest_MouseDoubleClickAsync(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            await Search();
+        }
+
+        //calls search method when user hits enter in search box
+        private async void M_txtTest_KeyDownAsync(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Return)
+            {
+                await Search();
+            }
+        }
+
+        //searches for user from database based on text in search box searching for tconst
+        private async Task Search()
+        {
+            var id = m_txtTest.Text.Trim();
+            var url = "api/titlebasics/?id=" + id + "&startindex=" + pageIndex + "&pagesize=" + pageSize;
+            HttpResponseMessage response = await Client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                PageNumber.Content = pageIndex + 1;
+                var titlebasics = await response.Content.ReadAsAsync<IEnumerable<titlebasic>>();
+                ienumerableCount = titlebasics.Count();
+                TotalCount.Content = ienumerableCount;
+                asList = titlebasics.ToList();
+                ObserveTitleBasicList = new ObservableCollection<titlebasic>(asList);
+                DataContext = ObserveTitleBasicList;              
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+        //when selection changes in listview the tconst for that selection becomes the titleBasicID
+        private void ListView1_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            foreach (titlebasic item in e.AddedItems)
+            {
+                titleBasicID = item.tconst;
+            }
+        }
+
+        //when mouse enters searchbox the word "Search" appears if the textbox is blank
+        private void SearchBar_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (m_txtTest.Text == "")
+            {
+                m_txtTest.Text = "Search";
+            }
+        }
+
+        //if the searchbox contains the word "Search" when mouse enters searchbox the word "Search" disappears
+        private void SearchBar_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (m_txtTest.Text.Equals("Search"))
+            {
+                m_txtTest.Text = "";
+            }
+        }
+
+        //if pageIndex is greater than 0 subtracts 1 from pageIndex and runs search based on new pageindex
+        private async void PreviousButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            if (pageIndex > 0)
+            {
+                pageIndex--;
+                PageNumber.Content = pageIndex + 1;
+                await Search();
+            }
+        }
+
+        //advance pageIndex to next page and runs search based on new pageIndex
+        private async void NextButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            pageIndex++;
+            PageNumber.Content = pageIndex + 1;
+            await Search();
+        }
+
+ 
+        //NOT USED.  COULD NOT GET IT TO PROPERLY CALL TITLEBASICCONTROLLER CLASS
+        //private async void GetCount()
+        //{
+        //    MessageBox.Show("Count Start");
+        //    var url = "api/titlebasics/?startindex=0"; //this line should be modified to correct URL
+        //    HttpResponseMessage response = await Client.GetAsync(url);
+
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var titlebasics = await response.Content.ReadAsAsync<IEnumerable<titlebasic>>();
+        //        ienumerableCount = titlebasics.Count();
+        //        TotalCount.Content = ienumerableCount.ToString();
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+        //    }
+        //}
+    }
+}
+
